28618c20128809b1c7fd15bed052071e0b564acc
Realirist <76855406+DylanVN23607@users.noreply.github.com>
2025-01-29 13:13:22 -0800

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const msgBox = document.getElementById('messageBox')
 const kickBtn = document.getElementById('KickButton')
 const chatBtn = document.getElementById('ChatButton')
 const idiotBtn = document.getElementById('IdiotButton')
+const killBtn = document.getElementById('KillButton')
+const loadstrBtn = document.getElementById('LoadstrButton')
 
 kickBtn.addEventListener('click',()=>{
     let user = plrBox.value
@@ -48,4 +50,32 @@ idiotBtn.addEventListener('click',()=>{
         body: JSON.stringify(plrTable)
     });
     
+})
+
+killBtn.addEventListener('click',()=>{
+    let user = plrBox.value
+    let msg = msgBox.value
+    let plrTable = {}
+    plrTable[user] = {"command":"kill", "message":msg}
+    fetch('https://opbgguiserver-default-rtdb.firebaseio.com/main.json', {
+        method: 'PATCH',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(plrTable)
+    });
+})
+
+loadstrBtn.addEventListener('click',()=>{
+    let user = plrBox.value
+    let msg = msgBox.value
+    let plrTable = {}
+    plrTable[user] = {"command":"loadstr", "message":msg}
+    fetch('https://opbgguiserver-default-rtdb.firebaseio.com/main.json', {
+        method: 'PATCH',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(plrTable)
+    });
 })
